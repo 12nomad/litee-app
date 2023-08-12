8b014787f5931a077a720cdfb2b863e41527cf5a
Jo Harijaonina <jo.harijaonina@gmail.com>
2023-08-12 16:50:22 +0300

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,21 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [process.env.CLIENT_URL!],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      credentials: true,
+    },
+  });
 
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  app.use(cookieParser());
-  app.enableCors({
-    origin: [process.env.CLIENT_URL!],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
 
   await app.listen(Number(process.env.PORT) || 6969);
 }
